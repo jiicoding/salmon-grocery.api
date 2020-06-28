9a9f2e9d05f1a18114a6dedcf58b736b74097134
nxa127 <nxa127@gmail.com>
2020-06-28 23:47:48 +0700

    get products api

--- src/api/product/product.controller.js
+++ src/api/product/product.controller.js
@@ -1,70 +1,107 @@
-const { insertProduct, insertProductImage } = require('./product.service');
+const {
+  insertProduct,
+  insertProductImage,
+  getProducts,
+  getMediaById,
+} = require('./product.service');
 const fs = require('fs');
-const path = require('path');
 
 module.exports = {
-  addProductToDb: (req, res) => {
+  addProductToDb: async (req, res) => {
     const body = req.body;
     const images = req.files || [];
     const { product_name, type, price } = body;
     if (!product_name) {
       return res.status(400).json({
         success: false,
-        error: 'product name can not be empty or null.',
+        error: {
+          code: 'EMPTY_PRODUCT_NAME',
+          message: 'product name can not be empty or null.',
+        },
       });
     }
 
     if (!type) {
       return res.status(400).json({
         success: false,
-        error: 'type can not be empty or null.',
+        error: {
+          code: 'EMPTY_TYPE',
+          message: 'type can not be empty or null.',
+        },
       });
     }
 
     if (!price) {
       return res.status(400).json({
         success: false,
-        error: 'price can not be empty or null.',
+        error: {
+          code: 'EMPTY_PRICE',
+          message: 'price can not be empty or null.',
+        },
       });
     }
 
-    insertProduct(body, (err, results) => {
-      if (err) {
-        return res.status(500).json({
-          success: false,
-          error: err,
-        });
-      }
+    const result = await insertProduct(body);
 
-      let uploadedImageError = [];
+    if (!result.success) {
+      return res.status(400).json(result);
+    }
 
-      images.forEach((img) => {
-        let imageName = img.originalname || '';
-        imageName = imageName.trim().replace(/ /g, '-');
-        let imagePath = img.path;
-        imagePath = `${imagePath}-${imageName}`;
-        fs.renameSync(img.path, imagePath);
-        insertProductImage(
-          {
-            productId: results.insertId,
-            url: imagePath,
-          },
-          (err, results) => {
-            if (err) {
-              uploadedImageError.push(err);
-            }
-          }
-        );
+    const { id } = result.data;
+
+    const saveProductMedia = images.map(async (img) => {
+      let imageName = img.originalname || '';
+      imageName = imageName.trim().replace(/ /g, '-');
+      let imagePath = img.path;
+      imagePath = `${imagePath}-${imageName}`;
+      fs.renameSync(img.path, imagePath);
+      const saveImageResult = await insertProductImage({
+        productId: id,
+        url: imagePath,
+        image_name: imageName,
       });
 
-      return res.status(200).json({
-        success: true,
-        media: {
-          success: !!uploadedImageError.length,
-          error: uploadedImageError,
-        },
-        data: results,
-      });
+      return saveImageResult;
+    });
+
+    const saveMediaResult = await Promise.all(saveProductMedia);
+
+    return res.status(200).json({
+      ...result,
+      data: {
+        ...result.data,
+        imageUploaded: saveMediaResult,
+      },
+    });
+  },
+  getProducts: async (req, res) => {
+    const { page: reqPage, size: reqSize } = req.query;
+    let page = 0;
+    let size = 10;
+
+    if (reqPage && reqPage > 0 && parseInt(reqPage) !== NaN) {
+      page = Math.floor(reqPage);
+    }
+
+    if (reqSize && reqSize > 0 && parseInt(reqSize) !== NaN) {
+      size = Math.floor(reqSize);
+    }
+
+    const products = await getProducts({ size, page });
+
+    const getProductMedia = products.map(async (prod) => {
+      const images = await getMediaById(prod.id);
+      return {
+        ...prod,
+        images,
+      };
+    });
+
+    const data = await Promise.all(getProductMedia);
+
+    return res.status(200).json({
+      success: true,
+      data,
     });
   },
 };
--- src/api/product/product.service.js
+++ src/api/product/product.service.js
@@ -1,34 +1,69 @@
 const pool = require('../../config/database');
+const path = require('path');
 
 module.exports = {
-  insertProduct: (data, callback) => {
-    pool.query(
-      `insert into Products(product_name, description, type, price) values (?,?,?,?)`,
-      [data.product_name, data.description, data.type, data.price],
-      (error, results) => {
-        if (error) {
-          return callback({
-            code: error.code,
-            message: error.sqlMessage,
-          });
-        }
-        return callback(null, results);
-      }
-    );
+  insertProduct: async (data) => {
+    try {
+      const result = await pool.query(
+        `insert into Products(product_name, description, type, price) values (?,?,?,?)`,
+        [data.product_name, data.description, data.type, data.price]
+      );
+      const { insertId } = result[0];
+      return {
+        success: true,
+        data: {
+          id: insertId,
+        },
+      };
+    } catch (error) {
+      return {
+        success: false,
+        error: {
+          code: error.code,
+          message: error.message,
+        },
+      };
+    }
   },
-  insertProductImage: (data, callback) => {
-    pool.query(
-      `insert into ProductsMedia(product_id, image_url) values(?,?)`,
-      [data.productId, data.url],
-      (error, results) => {
-        if (error) {
-          return callback({
-            code: error.code,
-            message: error.sqlMessage,
-          });
-        }
-        return callback(null, results);
-      }
+  insertProductImage: async (data) => {
+    try {
+      await pool.query(
+        `insert into ProductsMedia(product_id, image_url) values(?,?)`,
+        [data.productId, data.url]
+      );
+      return {
+        success: true,
+        image: data.image_name,
+      };
+    } catch (error) {
+      return {
+        success: false,
+        error: {
+          code: error.code,
+          message: error.message,
+        },
+      };
+    }
+  },
+  getProducts: async (data) => {
+    const { size, page } = data;
+    const results = await pool.query(
+      `SELECT * FROM Products LIMIT ${(page - 1) * size}, ${size};`
     );
+    if (!results[0].length) {
+      return [];
+    }
+    return results[0];
+  },
+  getMediaById: async (id) => {
+    const results = await pool.query(
+      `SELECT image_url FROM ProductsMedia WHERE product_id=?;`,
+      [id]
+    );
+    if (!results[0].length) {
+      return [];
+    }
+    const mapImages = results[0].map((img) => path.resolve(img.image_url));
+    return mapImages;
   },
 };
