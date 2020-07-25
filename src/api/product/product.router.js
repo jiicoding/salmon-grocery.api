e35ebe492b64c5cf34c07a0985eeb15a3e08b963
nxa127 <nxa127@gmail.com>
2020-07-25 18:26:05 +0700

    upload image

--- src/api/product/product.router.js
+++ src/api/product/product.router.js
@@ -5,17 +5,12 @@ const {
   updateProductById,
 } = require('./product.controller');
 const router = require('express').Router();
-const { checkToken, checkAdminToken } = require('../../utils/auth/tokenValidation');
-const multer = require('multer');
-
-const uploader = multer({ dest: 'src/public/upload' });
-
-router.post(
-  '/',
+const {
+  checkToken,
   checkAdminToken,
-  uploader.array('media', process.env.MAX_UPLOADED_IMAGE),
-  addProductToDb
-);
+} = require('../../utils/auth/tokenValidation');
+
+router.post('/', checkAdminToken, addProductToDb);
 
 router.get('/', checkToken, getProducts);
 router.get('/:productId', checkToken, getProductById);
